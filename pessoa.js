4c4dd3daf95dcceb87842928c0eb692ea82c3bea
zlatanpaula-oss <zlatan.paula@escola.pr.gov.br>
2026-08-12 10:22:11 -0300

    Create pessoa.js

---
+++ pessoa.js
@@ -0,0 +1,17 @@
+class Pessoa{
+  constructor (nome, idade){
+    this. nome = nome;
+    this.idade = idade;
+  }
+  
+  imprimirDetalhes(){
+    `
+    return 
+    nome:${this.Nome}
+    idade:${this.Idade}
+  `
+}
+
+
+
+}
